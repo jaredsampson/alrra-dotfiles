5a943d81c1a88553836cd6d60cf075301a1cdc30
Jared Sampson <jared.sampson@gmail.com>
2020-01-02 09:52:45 -0500

    Add a few QuickLook plugins.
    QLStephen, ColorCode, CSV

--- src/os/install/macos/quick_look.sh
+++ src/os/install/macos/quick_look.sh
@@ -8,5 +8,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Quick Look\n\n"
 
+brew_install "Stephen" "qlstephen" "homebrew/cask" "cask"
+brew_install "ColorCode" "qlcolorcode" "homebrew/cask" "cask"
+brew_install "CSV" "quicklook-csv" "homebrew/cask" "cask"
 brew_install "JSON" "quicklook-json" "homebrew/cask" "cask"
 brew_install "Markdown" "qlmarkdown" "homebrew/cask" "cask"
