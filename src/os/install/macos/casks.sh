b9063205bcc9c1876e489b43b7ca79ae74f2e42b
Jared Sampson <jared.sampson@gmail.com>
2020-01-08 15:13:26 -0500

    Add Setapp and function to run the installer.

--- src/os/install/macos/casks.sh
+++ src/os/install/macos/casks.sh
@@ -24,6 +24,34 @@ install_homebrew() {
         "Install Homebrew"
 }
 
+
+setapp_is_installed() {
+    [ -d "/Applications/Setapp.app" ]
+}
+
+get_setapp_version() {
+    brew cask info setapp | grep "setapp:" | awk '{print $2}'
+}
+
+finish_setapp_installation() {
+
+    local version=$(get_setapp_version)
+    local installer="/usr/local/Caskroom/setapp/$version/Install Setapp.app"
+
+    if ! [ setapp_is_installed ]; then
+        if [ -d "$installer" ]; then
+            open "$installer"
+        fi
+    fi
+
+    execute \
+        "until setapp_is_installed; do \
+            sleep 5; \
+         done" \
+        "Finish Setapp installation"
+
+}
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
@@ -32,6 +60,8 @@ main() {
 
     install_homebrew
     brew bundle --no-lock --file=Brewfile-casks
+
+    finish_setapp_installation
 
 }
 
